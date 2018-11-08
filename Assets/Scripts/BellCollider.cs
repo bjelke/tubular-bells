64d41b0fbbbc76994fe40397a5e637c1518371c5
bjelke <bljelke64@gmail.com>
2018-11-07 21:18:10 -0600

    adding primitive objects in generateChimes. starting collision stuff between chimes and controllers but steamvr changed how input works so its gonna take some work

---
+++ Assets/Scripts/BellCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BellCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //TODO: put controllers on their own layer, then check if other.gameObject.layer is that layer
+
+        // if a controller has collided with a chime, play the appropriate sound
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("ControllerLayer"))
+        {
+            this.gameObject.GetComponent<Renderer>().material.color = new Color(0, 0, 200);
+            Debug.Log("inside method");
+        }
+    }
+}
---
+++ Assets/Scripts/GenerateChimes.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GenerateChimes : MonoBehaviour {
+
+    public GameObject chimes;
+
+    //List<GameObject> cylinders = new List<GameObject>();
+    float[] chimeLengths = new float[18] { 38f, 39f, 41.5f, 44f, 45.5f, 48.5f, 51.5f, 54.5f, 56f, 59.5f, 63f, 40f, 43f, 46.5f, 49.5f, 52.5f, 57.5f, 61f};
+
+	// Use this for initialization
+	void Start () {
+        //convert chime lengths from inches to meters & create cylinder primitives.
+        for (int i = 0; i < 18; i++){
+            GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            cylinder.transform.parent = chimes.transform;
+            chimeLengths[i] = chimeLengths[i] / 39.7301f *2 / 3;
+
+            cylinder.AddComponent<BellCollider>();
+        }
+
+        PositionCylinders();
+        SaveCylinders();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PositionCylinders()
+    {
+        //for(int i = 0; i < 18; i++){
+        //    cylinders[i].transform.localScale = new Vector3(0.0254f, chimeLengths[i], 0.0254f);
+        //}
+        int chimeCount = 0;
+        foreach( Transform cylinder in chimes.transform){
+            cylinder.transform.localScale = new Vector3(0.0554f, chimeLengths[chimeCount], 0.0554f); //0.0254f
+            cylinder.transform.localPosition = new Vector3(0 + chimeCount*0.1f, 1.5f - chimeLengths[chimeCount]/2, 0);
+            chimeCount++;
+        }
+
+    }
+
+
+    public void SaveCylinders()
+    {
+
+    }
+
+}
